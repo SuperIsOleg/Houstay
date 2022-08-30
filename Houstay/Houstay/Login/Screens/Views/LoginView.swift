baa323549a63b23dd8a10ded55310c85caefebbd
Oleg Kalistratov <oleg.K@northitgroup.eu>
2022-08-30 16:17:49 +0300

    create logoIcon in LoginView

--- Houstay/Houstay/Login/Screens/Views/LoginView.swift
+++ Houstay/Houstay/Login/Screens/Views/LoginView.swift
@@ -8,14 +8,43 @@
 import UIKit
 
 class LoginView: UIView {
-
+    
+    private let logoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = R.image.logo()
+        return imageView
+    }()
+    
+    private let logoLabel: UILabel = {
+       let label = UILabel()
+        label.text = R.string.localizable.loginTitle()
+        label.font = R.font.robotoMedium(size: 24)
+        label.textColor = R.color.blue100()
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = R.color.blue100()
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setupLayout() {
+        self.backgroundColor = R.color.white500()
+        self.addSubview(logoImage)
+        logoImage.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(150)
+            $0.centerX.equalToSuperview()
+            $0.height.width.equalTo(90)
+        }
+        
+        self.addSubview(logoLabel)
+        logoLabel.snp.makeConstraints {
+            $0.top.equalTo(logoImage.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
+        }
+    }
 }
