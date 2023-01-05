dc7d23172a817b2705f78028a4efc9a3fbc0f3e1
Oleg Kalistratov <olegkalistratov94@gmail.com>
2023-01-05 18:59:22 +0300

    edit model

--- Houstay/Houstay/Application/Protocol/HomeItemsProtocol.swift
+++ Houstay/Houstay/Application/Protocol/HomeItemsProtocol.swift
@@ -8,21 +8,21 @@
 import Foundation
 
 protocol HomeItemsProtocol {
-    var userId: String { get set }
-    var email: String { get set }
-    var numberOfTelefon: String { get set }
-    var avatarLink: String { get set }
-    var userName: String { get set }
     var address: String { get set }
     var appartementsId: String { get set }
+    var avatarLink: String { get set }
     var city: String { get set }
-    var facilities: [String] { get set }
-    var price: Int { get set }
-    var imagesLink: [String] { get set }
-    var favorite: Bool { get set }
-    var period: String { get set }
-    var views: Int { get set }
-    var review: [String] { get set }
-    var publicationDate: Date { get set }
     var descriptions: String { get set }
+    var email: String { get set }
+    var facilities: [String] { get set }
+    var favorite: Bool { get set }
+    var imagesLink: [String] { get set }
+    var numberOfTelefon: String { get set }
+    var period: String { get set }
+    var price: Int { get set }
+    var publicationDate: Date { get set }
+    var review: [String] { get set }
+    var userId: String { get set }
+    var userName: String { get set }
+    var views: Int { get set }
 }
--- Houstay/Houstay/Home/Model/HomeItemsModel.swift
+++ Houstay/Houstay/Home/Model/HomeItemsModel.swift
@@ -8,42 +8,42 @@
 import Foundation
 
 struct HomeItemsModel: HomeItemsProtocol, Codable, Hashable {
-    var userId: String
-    var email: String
-    var numberOfTelefon: String
-    var avatarLink: String
-    var userName: String
     var address: String
     var appartementsId: String
+    var avatarLink: String
     var city: String
-    var facilities: [String]
-    var price: Int
-    var imagesLink: [String]
-    var favorite: Bool
-    var period: String
-    var views: Int
-    var review: [String]
-    var publicationDate: Date
     var descriptions: String
+    var email: String
+    var facilities: [String]
+    var favorite: Bool
+    var imagesLink: [String]
+    var numberOfTelefon: String
+    var period: String
+    var price: Int
+    var publicationDate: Date
+    var review: [String]
+    var userId: String
+    var userName: String
+    var views: Int
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.userId = try container.decode(String.self, forKey: .userId)
-        self.email = try container.decode(String.self, forKey: .email)
-        self.numberOfTelefon = try container.decode(String.self, forKey: .numberOfTelefon)
-        self.avatarLink = try container.decode(String.self, forKey: .avatarLink)
-        self.userName = try container.decode(String.self, forKey: .userName)
         self.address = try container.decode(String.self, forKey: .address)
         self.appartementsId = try container.decode(String.self, forKey: .appartementsId)
+        self.avatarLink = try container.decode(String.self, forKey: .avatarLink)
         self.city = try container.decode(String.self, forKey: .city)
-        self.facilities = try container.decode([String].self, forKey: .facilities)
-        self.price = try container.decode(Int.self, forKey: .price)
-        self.imagesLink = try container.decode([String].self, forKey: .imagesLink)
-        self.favorite = try container.decode(Bool.self, forKey: .favorite)
-        self.period = try container.decode(String.self, forKey: .period)
-        self.views = try container.decode(Int.self, forKey: .views)
-        self.review = try container.decode([String].self, forKey: .review)
         self.descriptions = try container.decode(String.self, forKey: .descriptions)
+        self.email = try container.decode(String.self, forKey: .email)
+        self.facilities = try container.decode([String].self, forKey: .facilities)
+        self.favorite = try container.decode(Bool.self, forKey: .favorite)
+        self.imagesLink = try container.decode([String].self, forKey: .imagesLink)
+        self.numberOfTelefon = try container.decode(String.self, forKey: .numberOfTelefon)
+        self.period = try container.decode(String.self, forKey: .period)
+        self.price = try container.decode(Int.self, forKey: .price)
+        self.review = try container.decode([String].self, forKey: .review)
+        self.userId = try container.decode(String.self, forKey: .userId)
+        self.userName = try container.decode(String.self, forKey: .userName)
+        self.views = try container.decode(Int.self, forKey: .views)
         
         do {
             self.publicationDate = try container.decode(String.self, forKey: .publicationDate).convertToDate() ?? Date()
@@ -51,6 +51,6 @@ struct HomeItemsModel: HomeItemsProtocol, Codable, Hashable {
             self.publicationDate = Date()
             print(error.localizedDescription)
         }
-    }
 
+    }
 }
